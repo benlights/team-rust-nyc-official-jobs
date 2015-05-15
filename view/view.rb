ffb9be497338769903fd22f589106c7d5a59fad7
michaelbbozza <michaelbbozza@gmail.com>
2015-05-15 14:43:16 -0400

    further into file

--- view/view.rb
+++ view/view.rb
@@ -1,16 +1,19 @@
 class View
-  # attr_reader
+  attr_reader :jobs
   def self.welcome
-    puts "Welcome to our "
+    puts "Welcome to our program, created by Team Rust. Now, let's shake off some of the Rust and get searching for some jobs!"
+    sleep 1.5
     puts "What type of job would you like to search for?"
+    View.input
   end
 
   def self.input
     job = gets.chomp
   end
 
-  def self.display
-
+  def self.display(job)
+    puts job
   end
-
 end
+
+View.welcome
