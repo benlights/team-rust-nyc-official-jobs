ac4d92142754bc1b4760e001fcae5fedf594c9e8
Mendel Schneerson <mmschneerson@gmail.com>
2015-05-15 14:32:35 -0400

    finished

---
+++ api_module.rb
@@ -0,0 +1,11 @@
+require 'open-uri'
+require 'json'
+require_relative "models/restaurant"
+
+module ApiGetter
+
+  def self.get_api(description)
+    JSON.parse(open("https://data.cityofnewyork.us/resource/kpav-sd4t.json?$q=#{description}&$order=salary_range_from%20DESC&$limit=10").read)
+  end
+
+end
