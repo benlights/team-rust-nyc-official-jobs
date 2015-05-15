9c59474375dd4e6659477a123dca96a64db20c61
michaelbbozza <michaelbbozza@gmail.com>
2015-05-15 14:32:01 -0400

    added view folder and part of view file

---
+++ view/view.rb
@@ -0,0 +1,16 @@
+class View
+  # attr_reader
+  def self.welcome
+    puts "Welcome to our "
+    puts "What type of job would you like to search for?"
+  end
+
+  def self.input
+    job = gets.chomp
+  end
+
+  def self.display
+
+  end
+
+end
